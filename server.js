419a67290abd28b6b5ce78a370bd9b04b297eaf6
Archit Gupta <archit.gupta@booking.com>
2018-12-15 16:56:30 +0100

    added babel watch

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('<html> <head> </head> <body> <h1> Hello World</h1></body></html>');
+    res.send('<html> <head> </head> <body> <h1> Hello Archit</h1></body></html>');
 } );
 
 server.listen(4000, () => {
